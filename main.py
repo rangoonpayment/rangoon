98b9d421c3b4b91b681b767ab2a49c0f5927bd4e
rangoonpayment <154636052+rangoonpayment@users.noreply.github.com>
2023-12-23 03:52:44 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from twilio.rest import Client
 
 # Your Twilio Account SID, Auth Token, and Twilio phone number
-account_sid = 'your_account_sid'
-auth_token = 'your_auth_token'
-twilio_phone_number = 'your_twilio_phone_number'
+account_sid = 'ACa000705d5520b9e2d8975b1a404248f6'
+auth_token = '2f3b01dc2f7b23f58fe51322e23598d2'
+twilio_phone_number = '+12057758770'
 
 # Recipient's phone number
-recipient_phone_number = 'recipient_phone_number'
+recipient_phone_number = '+959 449169428'
 
 # Create Twilio client
 client = Client(account_sid, auth_token)
